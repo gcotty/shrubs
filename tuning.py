81d5c7295996b601b4b39439897b9167d2d8e67c
Grant Cottingham <54192928+gcotty@users.noreply.github.com>
2019-08-29 10:43:22 -0500

    Update tuning.py

--- tuning.py
+++ tuning.py
@@ -1,1 +1,15 @@
+import pandas as pd
+
+# One hot encoding function for categorical data
+def encode(df):
+	feat_objs = [col for col in df if df[col].dtype == 'object']
+	feat_encs = [pd.get_dummies(df[i], prefix=i) for i in feat_objs]
+
+	for i in feat_encs:
+		df = df.join(i)
+	
+	for i in feat_objs:
+		df = df.drop([i], axis=1)
+
+	return df
 
